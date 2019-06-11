b9b150ef954bf4c539610f87089613c37ba81916
Alexander Pakter <alexander.pakter1@ge.com>
2019-06-11 23:13:05 +0000

    fixes to setup-docker.sh

--- setup-docker.sh
+++ setup-docker.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-set -e
+
+# This script will install docker on a CentOS / RHEL Linux (such as Predix DevBox).
+# Is will also set the docker permissions to allow the user who run this to 
+# user docker commands without being root.
+
+# The following command will remove docker from the system if it exists:
+# It is saved in this script ifor reference just in case you need to reinstall 
+# docker
+#
+# yum remove docker docker-client docker-client-latest docker-common \
+#                  docker-latest docker-latest-logrotate docker-logrotate \
+#                  docker-selinux docker-engine-selinux docker-engine
+
 
 # Check to see if we are running as root; if not exit
 uid=$(id | sed 's/^uid=\([0-9][0-9]*\).*/\1/' )
@@ -17,13 +29,20 @@ if [ $rv -ne 127 ] ; then
 	exit 0
 fi
 
+# This setting causes the script to exit if any errors occur
+set -e
+
 # Install Docker
 yum -y install docker
-groupadd docker
-chown root:docker /var/run/docker.sock
-usermod -aG docker `whoami`
+systemctl enable docker
+systemctl start docker
+user_grp=$(groups $(logname) | cut -d : -f 1)
+chown root:${user_grp} /var/run/docker.sock
 
-echo Docker setup completed successfully.  However, you will need to 
-echo completely log out and log back in again to finish the setup.
-echo
-echo Your environment may not work correctly until you log out and back in.
+# Or to do the chown with the docker group, use the code below
+# groupadd docker
+# chown root:docker /var/run/docker.sock
+# groupadd docker
+# usermod -aG docker $(logname)
+# newgrp docker $(logname)
+# newgrp $user_grp $(logname)
